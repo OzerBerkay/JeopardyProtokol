78d1773aec5793c8daf57d83f310e11b63b05dc3
Çağrı <cagri-ustun@hotmail.com>
2020-04-19 15:16:30 +0300

    update
    buton kontrolleri saglandi.

--- JeopardyProtocol/src/Jeopardy_Protocol/TCP_Client.java
+++ JeopardyProtocol/src/Jeopardy_Protocol/TCP_Client.java
@@ -5,7 +5,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-
 public class TCP_Client {
 
     private Socket clientSocket;
@@ -14,8 +13,16 @@ public class TCP_Client {
     private javax.swing.JTextPane historyJTextPane;
     private javax.swing.JLabel nameJLabel;
     private Thread clientThread;
+    private javax.swing.JButton abut;//Ömer Faruk Küçüker
+    private javax.swing.JButton bbut;
+    private javax.swing.JButton cbut;
+    private javax.swing.JButton dbut;
+    String LastSender = "th:";//Çağrı Üstün
 
-    protected void start(String host, int port, javax.swing.JTextPane jTextPaneHistory, javax.swing.JLabel jLabelName) throws IOException {
+    protected void start(String host, int port, javax.swing.JTextPane jTextPaneHistory,
+            javax.swing.JLabel jLabelName, javax.swing.JButton Abutton,
+            javax.swing.JButton Bbutton, javax.swing.JButton Cbutton,
+            javax.swing.JButton Dbutton) throws IOException {//Ömer Faruk Küçüker
         // client soketi oluşturma (ip + port numarası)
         clientSocket = new Socket(host, port);
 
@@ -24,6 +31,11 @@ public class TCP_Client {
         // client arayüzündeki isim yazısı, client ismi server tarafından belirlenecek
         this.nameJLabel = jLabelName;
 
+        //Butonlarımızı arayüzdekiler ile eşleştirdik.
+        this.abut = Abutton;
+        this.bbut = Bbutton;
+        this.cbut = Cbutton;
+        this.dbut = Dbutton;
         // input  : client'a gelen mesajları okumak için
         // output : client'dan bağlı olduğu server'a mesaj göndermek için
         clientOutput = new ObjectOutputStream(clientSocket.getOutputStream());
@@ -44,9 +56,42 @@ public class TCP_Client {
         clientOutput.writeObject(message);
     }
 
-    protected void writeToHistory(Object message) {
+    protected void openButtons() { //Ömer Faruk Küçüker
+        this.abut.setEnabled(true);
+        this.bbut.setEnabled(true);
+        this.cbut.setEnabled(true);
+        this.dbut.setEnabled(true);
+    }
+
+    protected void closeButtons() {//Ömer Faruk Küçüker
+        this.abut.setEnabled(false);
+        this.bbut.setEnabled(false);
+        this.cbut.setEnabled(false);
+        this.dbut.setEnabled(false);
+    }
+
+    protected void writeToHistory(Object message) {//Çağrı Üstün
         // client arayüzündeki history alanına mesajı yaz
+
+        String mes = message.toString();
+        if (mes.contains("Soru")) {// Bir soru geldiğinde butonlar aktif olur
+            historyJTextPane.setText("");
+            openButtons();
+        } else if (mes.contains("Yanlis cevap!")) { // cevap yanlışsa tüm clientların butonları aktif olur
+            openButtons();
+            if (nameJLabel.getText().equals(LastSender)) { //yanlış veren kullanıcının butonları aktif olmaz
+                closeButtons();
+            }
+        } else if (mes.contains("Dogru cevap!") || mes.contains("Cevap Kontrol Ediliyor Lütfen Bekleyiniz...")) {// doğru cevap verildiğinde sıradaki soru için herkesin butonları kapatılır.
+            closeButtons();
+        } else if (mes.contains("Kimse Bilemedi!")) {// ikinci cevapta da yanlış cevap verilirse sonraki soru için herkesin butonları kapatılır.
+            closeButtons();
+        }
         historyJTextPane.setText(historyJTextPane.getText() + "\n" + message);
+        if (mes.contains(":") && !mes.contains("Server")) { //en son mesaj yollayan kullanıcının kim olduğu belirlenir
+            LastSender = mes.substring(0, mes.indexOf(':')).trim();
+            System.out.println(LastSender);
+        }
     }
 
     protected void disconnect() throws IOException {
